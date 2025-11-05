bdc46ce4bd2589524254a5cca85eaf5292458704
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-11-05 22:01:50 +0100

    chore: bump spec (#15)
    * bump spec
    
    * bump interface
    
    * update interface
    
    * bump interfaces

--- src/interfaces/ILinkingUSD.sol
+++ src/interfaces/ILinkingUSD.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-interface ILinkingUSD {
+import {ITIP20} from "./ITIP20.sol";
+
+interface ILinkingUSD is ITIP20 {
     error TransfersDisabled();
+
+    function TRANSFER_ROLE() external view returns (bytes32);
+
+    function RECEIVE_WITH_MEMO_ROLE() external view returns (bytes32);
 }
