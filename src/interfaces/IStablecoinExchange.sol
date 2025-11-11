0954388079a8b7cbe85140d28c840779734bc982
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-11-11 11:54:46 +0100

    chore: sync specs (#21)
    * bump nonce
    
    * sync

--- src/interfaces/IStablecoinExchange.sol
+++ src/interfaces/IStablecoinExchange.sol
@@ -2,10 +2,7 @@
 pragma solidity ^0.8.13;
 
 interface IStablecoinExchange {
-    // Events
-    event OrderPlaced(
-        uint128 indexed orderId, address indexed maker, address indexed token, uint128 amount, bool isBid, int16 tick
-    );
+    error Unauthorized();
 
     event FlipOrderPlaced(
         uint128 indexed orderId,
@@ -16,20 +13,67 @@ interface IStablecoinExchange {
         int16 tick,
         int16 flipTick
     );
-
     event OrderCancelled(uint128 indexed orderId);
-
     event OrderFilled(
         uint128 indexed orderId, address indexed maker, address indexed taker, uint128 amountFilled, bool partialFill
     );
+    event OrderPlaced(
+        uint128 indexed orderId, address indexed maker, address indexed token, uint128 amount, bool isBid, int16 tick
+    );
+    event PairCreated(bytes32 indexed key, address indexed base, address indexed quote);
 
-    // Balance management
-    function withdraw(address token, uint128 amount) external;
+    function MAX_PRICE() external view returns (uint32);
+
+    function MAX_TICK() external view returns (int16);
+
+    function MIN_PRICE() external view returns (uint32);
+
+    function MIN_TICK() external view returns (int16);
+
+    function PRICE_SCALE() external view returns (uint32);
+
+    function activeOrderId() external view returns (uint128);
 
     function balanceOf(address user, address token) external view returns (uint128);
 
-    // Swap functions
-    // Will support routing across multiple pairs, in the future
+    function books(bytes32 pairKey)
+        external
+        view
+        returns (address base, address quote, int16 bestBidTick, int16 bestAskTick);
+
+    function cancel(uint128 orderId) external;
+
+    function createPair(address base) external returns (bytes32 key);
+
+    function executeBlock() external;
+
+    function getTickLevel(address base, int16 tick, bool isBid)
+        external
+        view
+        returns (uint128 head, uint128 tail, uint128 totalLiquidity);
+
+    function pairKey(address tokenA, address tokenB) external pure returns (bytes32 key);
+
+    function pendingOrderId() external view returns (uint128);
+
+    function place(address token, uint128 amount, bool isBid, int16 tick) external returns (uint128 orderId);
+
+    function placeFlip(address token, uint128 amount, bool isBid, int16 tick, int16 flipTick)
+        external
+        returns (uint128 orderId);
+
+    function priceToTick(uint32 price) external pure returns (int16 tick);
+
+    function quoteSwapExactAmountIn(address tokenIn, address tokenOut, uint128 amountIn)
+        external
+        view
+        returns (uint128 amountOut);
+
+    function quoteSwapExactAmountOut(address tokenIn, address tokenOut, uint128 amountOut)
+        external
+        view
+        returns (uint128 amountIn);
+
     function swapExactAmountIn(address tokenIn, address tokenOut, uint128 amountIn, uint128 minAmountOut)
         external
         returns (uint128 amountOut);
@@ -38,28 +82,7 @@ interface IStablecoinExchange {
         external
         returns (uint128 amountIn);
 
-    function quoteSwapExactAmountOut(address tokenIn, address tokenOut, uint128 amountOut)
-        external
-        view
-        returns (uint128 amountIn);
+    function tickToPrice(int16 tick) external pure returns (uint32 price);
 
-    function quoteSwapExactAmountIn(address tokenIn, address tokenOut, uint128 amountIn)
-        external
-        view
-        returns (uint128 amountOut);
-
-    // Order management
-    // Only supports placing an order on a pair between a token and its quote token
-    // Amount is denominated in the token
-    // Tick is the price of the token denominated in the quote token, minus 1, times 1000
-    // A bid is an order to buy the token using its quote token
-    // A ask is an order to sell the token for its quote token
-    function place(address token, uint128 amount, bool isBid, int16 tick) external returns (uint128 orderId);
-
-    // flipTick must be greater than tick if the order is a bid, less if the order is an ask
-    function placeFlip(address token, uint128 amount, bool isBid, int16 tick, int16 flipTick)
-        external
-        returns (uint128 orderId);
-
-    function cancel(uint128 orderId) external;
+    function withdraw(address token, uint128 amount) external;
 }
