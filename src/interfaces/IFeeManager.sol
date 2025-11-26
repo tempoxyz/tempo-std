6f88e6560991b4b2a052ca32506740e6cfb961a8
zerosnacks <zerosnacks@protonmail.com>
2025-11-26 17:58:04 +0100

    sync

--- src/interfaces/IFeeManager.sol
+++ src/interfaces/IFeeManager.sol
@@ -12,10 +12,13 @@ interface IFeeManager is IFeeAMM {
         uint256 maxAmount,
         uint256 actualUsed,
         address userToken,
-        address validatorToken
+        address validatorToken,
+        address feeRecipient
     ) external;
 
-    function collectFeePreTx(address user, address txToAddress, uint256 maxAmount) external returns (address userToken);
+    function collectFeePreTx(address user, address txToAddress, uint256 maxAmount, address feeRecipient)
+        external
+        returns (address userToken);
 
     function executeBlock() external;
 
