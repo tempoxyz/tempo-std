5ec8ac653ced663efb7634a29ee05e65a33ef3f1
0xrusowsky <0xrusowsky@proton.me>
2026-04-22 18:02:50 +0200

    chore: rename to TIP20_FACTORY

--- src/StdBase.sol
+++ src/StdBase.sol
@@ -32,14 +32,6 @@ abstract contract StdBase {
     IFeeManager public constant feeAMM = StdPrecompiles.TIP_FEE_MANAGER;
     address public constant FEE_AMM = StdPrecompiles.TIP_FEE_MANAGER_ADDRESS;
 
-    // TIP-403 registry precompile
-    ITIP403Registry public constant registry = StdPrecompiles.TIP403_REGISTRY;
-    address public constant REGISTRY = StdPrecompiles.TIP403_REGISTRY_ADDRESS;
-
-    // TIP-20 address registry precompile
-    IAddressRegistry public constant tip20Registry = StdPrecompiles.ADDRESS_REGISTRY;
-    address public constant TIP20_REGISTRY = StdPrecompiles.ADDRESS_REGISTRY_ADDRESS;
-
     // Validator config precompile
     IValidatorConfig public constant validatorConfig = StdPrecompiles.VALIDATOR_CONFIG;
     address public constant VALIDATOR_CONFIG = StdPrecompiles.VALIDATOR_CONFIG_ADDRESS;
@@ -48,13 +40,21 @@ abstract contract StdBase {
     IValidatorConfigV2 public constant validatorConfigV2 = StdPrecompiles.VALIDATOR_CONFIG_V2;
     address public constant VALIDATOR_CONFIG_V2 = StdPrecompiles.VALIDATOR_CONFIG_V2_ADDRESS;
 
-    // TIP-20 factory precompile
-    ITIP20Factory public constant factory = StdPrecompiles.TIP20_FACTORY;
-    address public constant FACTORY = StdPrecompiles.TIP20_FACTORY_ADDRESS;
-
     // Signature verifier precompile
     ISignatureVerifier public constant signatureVerifier = StdPrecompiles.SIGNATURE_VERIFIER;
     address public constant SIGNATURE_VERIFIER = StdPrecompiles.SIGNATURE_VERIFIER_ADDRESS;
+
+    // TIP-403 registry precompile
+    ITIP403Registry public constant registry = StdPrecompiles.TIP403_REGISTRY;
+    address public constant REGISTRY = StdPrecompiles.TIP403_REGISTRY_ADDRESS;
+
+    // TIP-20 address registry precompile
+    IAddressRegistry public constant tip20Registry = StdPrecompiles.ADDRESS_REGISTRY;
+    address public constant TIP20_REGISTRY = StdPrecompiles.ADDRESS_REGISTRY_ADDRESS;
+
+    // TIP-20 factory precompile
+    ITIP20Factory public constant tip20Factory = StdPrecompiles.TIP20_FACTORY;
+    address public constant TIP20_FACTORY = StdPrecompiles.TIP20_FACTORY_ADDRESS;
 
     // pathUSD is just a TIP20 at a special address (0x20C0...) with token_id=0
     ITIP20 public constant pathUSD = StdTokens.PATH_USD;
