467949c5ed640ae9174fd6ea02575135ee3d0e19
Derek <256792747+decofe@users.noreply.github.com>
2026-07-06 09:08:40 +0000

    Update stablecoin DEX ABI

--- src/interfaces/IStablecoinDEX.sol
+++ src/interfaces/IStablecoinDEX.sol
@@ -58,6 +58,7 @@ interface IStablecoinDEX {
     error BelowMinimumOrderSize(uint128 amount);
     error InvalidBaseToken();
     error OrderNotStale();
+    error IndexAlreadySet();
 
     event OrderCancelled(uint128 indexed orderId);
     event OrderFilled(
@@ -110,6 +111,10 @@ interface IStablecoinDEX {
         view
         returns (address base, address quote, int16 bestBidTick, int16 bestAskTick);
 
+    function bookIndexForKey(bytes32 bookKey) external view returns (bool set, uint32 index);
+
+    function bookKeyForIndex(uint32 index) external view returns (bytes32 bookKey);
+
     function cancel(uint128 orderId) external;
 
     function cancelStaleOrder(uint128 orderId) external;
@@ -132,6 +137,8 @@ interface IStablecoinDEX {
         returns (uint128 orderId);
 
     function priceToTick(uint32 price) external pure returns (int16 tick);
+
+    function setBookIndex(uint32 index) external;
 
     function quoteSwapExactAmountIn(address tokenIn, address tokenOut, uint128 amountIn)
         external
